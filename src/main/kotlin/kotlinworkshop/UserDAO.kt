6a9df2f88c4e57dba374f8ef716e75953d6135cb
Philipp Hauer <info@philipphauer.de>
2017-09-14 22:36:55 +0200

    remove relevant parts of the implementation, because that is the task of the workshop :-)

--- src/main/kotlin/kotlinworkshop/UserDAO.kt
+++ src/main/kotlin/kotlinworkshop/UserDAO.kt
@@ -1,61 +1,10 @@
 package kotlinworkshop
 
-import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Component
-import java.sql.ResultSet
 
 @Component
 class UserDAO(private val template: JdbcTemplate) {
 
-    fun findAllUsers() = template.query("SELECT * FROM users;", this::mapToUser)
-
-    private fun mapToUser(rs: ResultSet, rowNum: Int) = User(
-            id = rs.getInt("id")
-            , email = rs.getString("email")
-            , description = rs.getString("description")
-            , name = mergeNames(rs)
-            , role = if (rs.getBoolean("guest")) Role.GUEST else Role.USER
-            , platform = mapToPlatform(rs.getString("platform"))
-            , dateCreated = rs.getTimestamp("date_created").toInstant()
-            , state = State.valueOf(rs.getString("state"))
-    )
-
-    fun findUser(id: Int) = try {
-        template.queryForObject("SELECT * FROM users WHERE id = ?;", arrayOf(id), this::mapToUser)
-    } catch (e: EmptyResultDataAccessException) {
-        null
-    }
-
-    fun deleteUser(id: Int) = template.update("DELETE FROM users WHERE id = ?;", id)
-
-    fun addUsers(newUsers: List<User>) {
-        val args = newUsers.map { user -> toArgumentsArray(user) }
-        template.batchUpdate("INSERT INTO users (id, email, firstname, lastname, description, guest, platform, date_created, state) VALUES (?, ?, ?, ?, ?, ?, ?, FROM_UNIXTIME(?), ?);", args)
-    }
+    //start implementing findAllUsers() here by using template...
 }
-
-private fun toArgumentsArray(user: User) = arrayOf(
-        user.id, user.email, user.name, null,
-        user.description,
-        user.role == Role.GUEST,
-        user.platform.toString(),
-        user.dateCreated.epochSecond,
-        user.state.toString()
-)
-
-private fun mapToPlatform(platform: String?) = when(platform) {
-    "eu", "EU", "europe" -> Platform.EU
-    "na", "NA", "northamerica" -> Platform.NA
-    null -> null
-    else -> throw UserDAOException("Unknown platform $platform")
-}
-
-private fun mergeNames(rs: ResultSet): String? {
-    val firstname = rs.getString("firstname") ?: ""
-    val lastname = rs.getString("lastname") ?: ""
-    val name = "$firstname $lastname"
-    return if (name.isBlank()) null else name
-}
-
-class UserDAOException(message: String): RuntimeException(message)
--- src/main/kotlin/kotlinworkshop/UserUI.kt
+++ src/main/kotlin/kotlinworkshop/UserUI.kt
@@ -1,20 +1,15 @@
 package kotlinworkshop
 
 import com.vaadin.annotations.Push
-import com.vaadin.icons.VaadinIcons
 import com.vaadin.server.VaadinRequest
 import com.vaadin.spring.annotation.SpringUI
 import com.vaadin.ui.Alignment
 import com.vaadin.ui.Button
 import com.vaadin.ui.Grid
 import com.vaadin.ui.HorizontalLayout
-import com.vaadin.ui.Notification
 import com.vaadin.ui.TextField
 import com.vaadin.ui.UI
 import com.vaadin.ui.VerticalLayout
-import com.vaadin.ui.themes.ValoTheme
-import java.time.Instant
-import java.util.Random
 
 
 @SpringUI(path = "")
@@ -23,14 +18,8 @@ class UserUI(val dao: UserDAO): UI() {
 
     val searchAllButton = Button("Get All Users")
     val idSearchField = TextField("ID")
-    val table = Grid<User>(User::class.java).apply {
+    val table = Grid<Any>().apply { //change this to Grid<User>(User::class.java) after you created the User class
         setSizeFull()
-        addComponentColumn { person ->
-            Button(VaadinIcons.TRASH).apply {
-                addStyleName(ValoTheme.BUTTON_BORDERLESS)
-                addClickListener{ deleteUser(person.id) }
-            }
-        }
     }
     val activeEmailsButton = Button("Show Mails of Active Users")
     val generateUsersAmount = TextField("Users Amount")
@@ -46,63 +35,18 @@ class UserUI(val dao: UserDAO): UI() {
             setExpandRatio(table, 1f)
 
             searchAllButton.addClickListener {
-                refresh()
+                println("searchAllButton")
             }
             idSearchField.addValueChangeListener { e ->
-                searchUserById(e.value)
+                println("idSearchField: ${e.value}")
             }
             generateUsersButton.addClickListener {
-                generateUsers(generateUsersAmount.value)
-                refresh()
+                println("generateUsersButton: ${generateUsersAmount.value}")
             }
             activeEmailsButton.addClickListener {
-                showMailsOfActiveUsers()
+                println("showMailsOfActiveUsers")
             }
         }
     }
 
-    private fun showMailsOfActiveUsers() {
-        val emails = dao.findAllUsers().filter { it.state == State.ACTIVATED }.map { it.email }
-        Notification.show("The Mails: $emails")
-    }
-
-    private fun searchUserById(idInput: String) {
-        val id = idInput.toIntOrNull() ?: return
-        val user = dao.findUser(id)
-        if (user != null) {
-            table.setItems(user)
-        } else {
-            table.setItems(listOf())
-        }
-    }
-
-    private fun refresh() {
-        val users = dao.findAllUsers()
-        table.setItems(users)
-    }
-
-    private fun generateUsers(amountString: String) {
-        val amount = amountString.toIntOrNull() ?: 0
-        if (amount != 0) {
-            val dummyUsers = (1..amount).map { createDummyUser() }
-            dao.addUsers(dummyUsers)
-        }
-    }
-
-    private fun deleteUser(userId: Int){
-        val affectedRows = dao.deleteUser(userId)
-        Notification.show("Deleted rows: $affectedRows")
-        table.setItems(dao.findAllUsers())
-    }
 }
-
-private fun createDummyUser() = User(
-        id = Random().nextInt(99999)
-        , email = "dummy@gmail.com"
-        , description = "nice dummy"
-        , name = "Dummy"
-        , role = Role.USER
-        , platform = Platform.EU
-        , dateCreated = Instant.now()
-        , state = State.ACTIVATED
-)
